fdd0beec10d9df8d0766e420e5861e3a1533748c
ryan griffin <ryangriffin@ryans-MacBook-Pro.local>
2018-10-23 19:25:50 +1300

    checking parallels

--- server.js
+++ server.js
@@ -62,14 +62,14 @@ var authenticationRouter = require('./routes/authentication');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use('/posts', postsRouter);
 //app.use('/image', imageRouter);
@@ -86,14 +86,38 @@ app.use('/authentication', authenticationRouter);
 
 
 if (process.env.NODE_ENV === 'production') {
-    //Express will serve up production assets like main.js file
-    app.use(express.static('client/build'));
-    //Express will serve up html file if it doesn't recognize the route
-    const path = require('path');
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    });
+
+
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+
+
+
+
+  });
 }
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  next('error');
+});
 
 
 
