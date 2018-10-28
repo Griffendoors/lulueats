b43e2a6c5a8209ec94b5fdc9b4fe8a24d570d648
ryan griffin <ryangriffin@ryans-MacBook-Pro.local>
2018-10-28 15:57:30 +1300

    debugging

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.use('/authentication', authenticationRouter);
 
 if (process.env.NODE_ENV === 'production') {
 
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   // Handle React routing, return all requests to React app
   app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +'client/build/index.html'));
